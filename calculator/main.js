769837c5165bbdc384b2c6000fd26cb088623dd3
Roberto Hasa <hasa.roberto@live.it>
2023-04-29 17:21:33 +0200

    Added main function to calculate

---
+++ calculator/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let height = document.querySelector("#height");
+let weight = document.querySelector("#weight");
+
+function calculate() {
+    let calories = weight.value * 24 * 1.3;
+    let bmi = (weight.value / (height.value * height.value)).toFixed(2);
+    let bodyCondition;
+
+    if (bmi < 16) {
+        bodyCondition = "Sottopeso grave";
+    } else if (bmi <= 16.99) {
+        bodyCondition = "Sottopeso moderato";
+    } else if (bmi <= 18.49) {
+        bodyCondition = "Sottopeso lieve";
+    } else if (bmi <= 21.99) {
+        bodyCondition = "Normopeso";
+    } else if (bmi <= 29.99) {
+        bodyCondition = "Sovrappeso";
+    } else if (bmi <= 34.99) {
+        bodyCondition = "Obesità lieve (1° classe)";
+    } else if (bmi <= 39.99) {
+        bodyCondition = "Obesità moderata (2° classe)";
+    } else {
+        bodyCondition = "Obesità grave (3° classe)";
+    }
+
+    console.log(calories);
+    console.log(bmi);
+    console.log(bodyCondition);
+
+}
